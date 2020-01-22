b5f8b42aa3f730191f2e524bfe7c7dd95805c2ac
Todd McKenzie <toddmckenzie@hotmail.com>
2020-01-21 21:42:59 -0600

    setup virtual env

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/toddmckenzie/Documents/Lambda/django-udemy/udemy-django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
